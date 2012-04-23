585d30b25ed5028f3f13b6144f006e07833c26ae
sgm <sim0629@gmail.com>
2012-04-23 15:58:14 +0900

    application에서 auth 분리 error.html 추가

--- wsgi.py
+++ wsgi.py
@@ -21,15 +21,13 @@ db = con.sirc_db
 
 def application(environ, start_response):
 	if not config.OPEN:
-		return test(start_response, '점검 중')
+		return error(start_response, '503 Service Unavailable', '점검 중')
 
 	path = environ.get('PATH_INFO', '')
-	context = {}
 	
 	if path.startswith('/callback/'):
 		return callback(environ, start_response)
 
-	# auth
 	cookie = Cookie.SimpleCookie()
 	cookie.load(environ.get('HTTP_COOKIE', ''))
 	authorized = False
@@ -39,9 +37,7 @@ def application(environ, start_response):
 	if authorized:
 		session = sessions[0]
 	else:
-		(url, secret) = request_request_token()
-		start_response('200 OK', [('Refresh', '0; url=%s' % url), ('Content-Type', 'text/html; charset=utf-8'), ('Set-Cookie', '%s=%s' % (config.TOKEN_SECRET, secret))])
-		return ['<a href="%s">SNUCSE Login Required</a>' % url]
+		return auth(environ, start_response)
 	
 	if path.startswith('/update/'):
 		return update(environ, start_response, session)
@@ -49,6 +45,11 @@ def application(environ, start_response):
 		return send(environ, start_response, session)
 	else:
 		return default(environ, start_response, session)
+
+def auth(environ, start_response):
+	(url, secret) = request_request_token()
+	start_response('200 OK', [('Refresh', '0; url=%s' % url), ('Content-Type', 'text/html; charset=utf-8'), ('Set-Cookie', '%s=%s' % (config.TOKEN_SECRET, secret))])
+	return ['<a href="%s">SNUCSE Login Required</a>' % url]
 
 def callback(environ, start_response):
 	parameters = cgi.parse_qs(environ.get('QUERY_STRING', ''))
@@ -77,7 +78,7 @@ def callback(environ, start_response):
 	}
 	db.session.insert(data)
 	start_response('200 OK', [('Refresh', '0; url=/sgm'), ('Content-Type', 'text/html; charset=utf-8'), ('Set-Cookie', '%s=%s; path=/sgm' % (config.SESSION_ID, session_id))])
-	return ['<a href="/sgm">Go</a>']
+	return ['<a href="/sgm">%s Go</a>' % account]
 
 def update(environ, start_response, session):
 	context = {}
@@ -100,6 +101,7 @@ def default(environ, start_response, session):
 	db[TARGET].remove({'datetime': {'$lt': datetime.datetime.now() - datetime.timedelta(1)}})
 	logs = db[TARGET].find().sort('datetime')
 	db.session.update({'session_id': session['session_id']}, {'$set': {'datetime': datetime.datetime.now()}})
+	db.session.remove({'datetime': {'$lt': datetime.datetime.now() - datetime.timedelta(1)}})
 	logs = list(logs)
 	for log in logs:
 		log['message'] = cgi.escape(log['message'])
@@ -108,27 +110,23 @@ def default(environ, start_response, session):
 	return [render('chat.html', context)]
 
 def render(template, context):
-	return jinja2.Environment(loader=jinja2.FileSystemLoader(PATH + '/public_html')).get_template(template).render(context).encode('utf8')
+	return jinja2.Environment(loader=jinja2.FileSystemLoader(PATH + '/public_html')).get_template(template).render(context).encode('utf-8')
 
-def error(start_response):
-	start_response('404 Not Found', [])
-	return []
+def error(start_response, code='404 Not Found', message=''):
+	context = {}
+	context['message'] = message.decode('utf-8')
+	start_response(code, [('Content-Type', 'text/html; charset=utf-8')])
+	return [render('error.html', context)]
 
-def test(start_response, message):
-	start_response('200 OK', [('Content-Type', 'text/plain; charset=utf-8')])
-	return [message]
-
-def create_session_id():
+def create_session_id(): # TODO: 중복 체크
 	import string
 	import random
-
 	bag = string.ascii_uppercase + string.ascii_lowercase + string.digits
 	return ''.join(random.sample(bag * 24, 24))
 
 def request_request_token():
 	import oauth2
 	import httplib2
-	
 	consumer = oauth2.Consumer(config.CONSUMER_KEY, config.CONSUMER_SECRET)
 	request = oauth2.Request.from_consumer_and_token(consumer, http_url=config.REQUEST_URL, parameters={'oauth_callback': config.CALLBACK_URL})
 	request.sign_request(oauth2.SignatureMethod_HMAC_SHA1(), consumer, None)
@@ -139,10 +137,8 @@ def request_request_token():
 def request_access_token(oauth_token, oauth_token_secret, oauth_verifier):
 	import oauth2
 	import httplib2
-
 	token = oauth2.Token(oauth_token, oauth_token_secret)
 	token.set_verifier(oauth_verifier)
-
 	consumer = oauth2.Consumer(config.CONSUMER_KEY, config.CONSUMER_SECRET)
 	request = oauth2.Request.from_consumer_and_token(consumer, token=token, http_url=config.ACCESS_URL)
 	request.sign_request(oauth2.SignatureMethod_HMAC_SHA1(), consumer, token)
