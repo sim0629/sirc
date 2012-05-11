84fb95bfc7c32b2d2964f32ee4537364c1644e7a
sgm <sim0629@gmail.com>
2012-05-11 10:25:55 +0900

    mode 표시

--- bot.py
+++ bot.py
@@ -28,36 +28,45 @@ class SBot(ircbot.SingleServerIRCBot):
 		for target in self.db.collection_names():
 			if target.startswith('#'):
 				c.join(target)
+	
+	def on_mode(self, c, e):
+		nick = irclib.nm_to_n(e.source())
+		target = e.target()
+		if irclib.is_channel(target):
+			self._log(target, '!', '[%s] by <%s>.' % (" ".join(e.arguments()), nick))
+		else:
+			pass
 
 	def on_nick(self, c, e):
-		before = e.source().split('!')[0]
+		before = irclib.nm_to_n(e.source())
 		after = e.target()
 		for target, ch in self.channels.items():
 			if ch.has_user(before):
 				self._log(target, '!', '<%s> is now known as <%s>.' % (before, after))
 	
 	def on_join(self, c, e):
-		nick = e.source().split('!')[0]
+		nick = irclib.nm_to_n(e.source())
 		self._log(e.target(), '!', '<%s> has joined.' % nick)
 
 	def on_part(self, c, e):
-		nick = e.source().split('!')[0]
+		nick = irclib.nm_to_n(e.source())
 		self._log(e.target(), '!', '<%s> has left.' % nick)
 	
 	def on_quit(self, c, e):
-		nick = e.source().split('!')[0]
+		nick = irclib.nm_to_n(e.source())
 		for target, ch in self.channels.items():
 			if ch.has_user(nick):
 				self._log(target, '!', '<%s> has quit.' % nick)
 	
 	def on_kick(self, c, e):
-		nick_s = e.source().split('!')[0]
+		nick = irclib.nm_to_n(e.source())
 		nick_m = e.arguments()[0]
 		because_of = e.arguments()[1]
 		self._log(e.target(), '!', '<%s> was kicked by <%s> because of "%s".' % (nick_m, nick_s, because_of))
 	
 	def on_pubmsg(self, c, e):
-		self._log(e.target().lower(), e.source().split('!')[0], e.arguments()[0])
+		nick = irclib.nm_to_n(e.source())
+		self._log(e.target().lower(), nick, e.arguments()[0])
 
 	def _log(self, target, source, message):
 		data = {
